1ffeafacbf607f19e9e8f907cd3b260934d27a82
mafhcow <mafhcow@gmail.com>
2018-01-06 20:28:22 -0500

    Added NN with embedding layer, achieves 0.55 AUC

---
+++ bin/torchModel.py
@@ -0,0 +1,172 @@
+import time
+import json
+import sklearn
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.utils.data
+import random
+from torch.autograd import Variable
+from torch.nn.functional import relu
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.metrics import classification_report
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import GridSearchCV
+import torch.nn.functional as F
+
+dataPath = "../Data/champions.txt"
+
+def readData(dataPath):
+    with open(dataPath) as f:
+        x = []
+        for line in f:
+            contents = line.strip().split("\t")
+            x.append((contents[0].split(" "), contents[1].split(" "), contents[2]))
+    return x
+
+def getIdMapping(rawData):
+    champIds = {}
+    index = 0
+
+    for i in range(len(rawData)):
+        data = rawData[i][0] + rawData[i][1]
+        for d in data:
+            if d in champIds:
+                continue
+            else:
+                champIds[d] = index
+                index += 1
+
+    return champIds, index
+
+def convert(champId, champIds):
+    array = np.zeros(139)
+    array[champIds[champId]] = 1.0
+    return array
+
+def generateTrainTest(rawData, champIds, split = 0.25):
+    X = []
+    Y = []
+
+    Xtest = []
+    Ytest = []
+    
+    for i in range(len(rawData)):
+        champs = rawData[i][0] + rawData[i][1]
+        tmp = []
+        for j in range(10):
+            tmp.append(convert(champs[j], champIds))
+            
+        if random.random() < 1.0 - split:
+            X.append(np.concatenate(tmp))
+            Y.append(int(rawData[i][2]))
+        else:
+            Xtest.append(np.concatenate(tmp))
+            Ytest.append(int(rawData[i][2]))    
+
+    return X, Y, Xtest, Ytest
+
+
+class EmbeddingNN(nn.Module):
+    def __init__(self, input_size, hidden, dropout, embedding_dim):
+        super(EmbeddingNN, self).__init__()
+        self.linear = nn.Linear(embedding_dim * 10, hidden)
+        self.linear2 = nn.Linear(embedding_dim, embedding_dim)
+        self.out = nn.Linear(hidden, 2)
+        self.softmax = nn.LogSoftmax()
+        self.dropout = nn.Dropout(p = dropout)
+        self.conv = nn.Conv1d(1, embedding_dim, 139, stride=139)
+        self.embedding_dim = embedding_dim
+    
+    def forward(self, input):
+        (batch, _) = input.size()
+        input = input.view(batch, 1, 1390)
+        convlayer = self.dropout(self.conv(input))
+        convlayer = torch.transpose(convlayer, 1, 2)
+        convlayer = convlayer.contiguous().view(batch * 10, self.embedding_dim)
+        convlayer = self.dropout(relu(self.linear2(convlayer)))
+        convlayer = convlayer.view(batch, self.embedding_dim * 10)
+        
+        hidden = self.dropout(relu(self.linear(convlayer)))
+        #hidden = self.dropout(relu(self.linear2(hidden)))
+        out = self.out(hidden)
+        out = self.softmax(out)
+        return out
+
+
+rawData = readData(dataPath)
+champIds, index = getIdMapping(rawData)
+X, Y, Xtest, Ytest = generateTrainTest(rawData, champIds)
+
+def train_model(train_data, test_data, model):
+    optimizer = torch.optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr= 1e-5, weight_decay = 1e-6)
+    prev_time = time.time()
+    for epoch in range(50):
+        print("****************************************")
+        print("Epoch", epoch + 1)
+    
+        loss = run_epoch(train_data, True, model, optimizer)
+        print("Trained in:", time.time() - prev_time)
+        print("Loss:", loss)
+        accuracy = run_epoch(test_data, False, model, optimizer)
+        print("Tested in:", time.time() - prev_time)
+        print("Accuracy:", accuracy)
+        prev_time = time.time()
+
+import math
+
+def run_epoch(data, is_training, model, optimizer):
+    data_loader = torch.utils.data.DataLoader(
+        data,
+        batch_size=50,
+        shuffle=True,
+        num_workers=4,
+        drop_last=False)
+    
+    losses = []
+    true_labels = []
+    for batch in data_loader:
+        input = Variable(batch['input'])
+        output = Variable(batch['output'])
+        predictions = model(input)
+        if is_training:
+            loss = criterion(predictions, output)
+            loss.backward()
+            losses.append(loss.data[0])
+            optimizer.step()
+        else:
+            def predict(out):
+                predictions = []
+                for i in range(out.shape[0]):
+                    predictions.append(math.exp(out[i][1]))
+                    #if out[i][0] > out[i][1]:
+                    #    predictions.append(0)
+                    #else:
+                    #    predictions.append(1)
+                return predictions
+            guesses = predict(predictions.data.numpy())
+            losses.extend(guesses)
+            true_labels.extend(output.data.numpy())
+                
+    if is_training:
+        avg_loss = np.mean(losses)
+        return avg_loss
+    else:
+        accuracy = sklearn.metrics.roc_auc_score(true_labels, np.array(losses))
+        #accuracy = accuracy_score(np.array(losses), true_labels)
+        return accuracy
+
+input_train = torch.FloatTensor(X)
+input_test = torch.FloatTensor(Xtest)
+output_train = torch.LongTensor(Y)
+output_test = torch.LongTensor(Ytest)
+
+model = EmbeddingNN(len(input_train[0]), 300, 0.2, 300)
+criterion = nn.NLLLoss()
+
+train_data = [{'input': x, 'output': y} for (x,y) in zip(input_train, output_train)]
+test_data = [{'input': x, 'output': y} for (x,y) in zip(input_test, output_test)]
+
+train_model(train_data, test_data, model)
